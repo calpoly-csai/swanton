c26022690f1d8f2138cd493d9e0221bd6e8abfcc
Michael Fekadu <mike.fekadu@gmail.com>
2020-07-26 22:52:27 +0000

    add voice kit setup
    Co-Authored-By: chidiewenike <chidiewenike@users.noreply.github.com>

--- voice_kit_raspbian_setup.sh
+++ voice_kit_raspbian_setup.sh
@@ -1,1 +1,7 @@
+#Add AIY package repo:
 
+echo "deb https://packages.cloud.google.com/apt aiyprojects-stable main" | sudo tee /etc/apt/sources.list.d/aiyprojects.list
+
+#Add Google package keys:
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
