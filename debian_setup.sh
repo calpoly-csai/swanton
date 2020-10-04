02af1a25f2eb38a6daf68168bbaa07e3af0518e6
Michael Fekadu <mfekadu@users.noreply.github.com>
2020-10-04 13:51:47 -0700

    add docker install stuff

--- debian_setup.sh
+++ debian_setup.sh
@@ -39,3 +39,44 @@ sudo pip3 install --upgrade pip || : #
 
 echo "now get poetry https://python-poetry.org"
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
+
+
+#
+# docker
+#
+
+# https://docs.docker.com/engine/install/debian/
+
+echo "setup docker repository"
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+echo "install docker engine"
+
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+echo "now get docker-compose"
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose || :
+
+echo "install docker command-line completion"
+
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.27.4/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
